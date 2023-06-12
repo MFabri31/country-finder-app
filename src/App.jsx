6f8081aa9a04be1d8319cb78d4b406d819f247ee
MFabri31 <48441333+MFabri31@users.noreply.github.com>
2023-06-12 19:43:32 -0300

    Add navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,9 @@
+import { NavBar } from './components/NavBar'
+
 function App() {
 	return (
 		<>
-			<h1>Country Finder</h1>
+			<NavBar />
 		</>
 	)
 }
---
+++ src/components/NavBar.jsx
@@ -0,0 +1,11 @@
+import { Box, Text } from '@chakra-ui/react'
+
+export const NavBar = () => {
+	return (
+		<Box py='2em' px='1em' shadow='md'>
+			<Text fontSize='lg' fontWeight='extraBold'>
+				Where in the world?
+			</Text>
+		</Box>
+	)
+}
