945bd01f2d64b9903df9eaa62ea85e90e87c4a73
MFabri31 <48441333+MFabri31@users.noreply.github.com>
2023-06-15 17:19:33 -0300

    Modify the padding of navbar for desktop

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -2,7 +2,7 @@ import { Box, Text } from '@chakra-ui/react'
 
 export const NavBar = () => {
 	return (
-		<Box py='2em' px='1em' shadow='md'>
+		<Box py='2em' px={{ base: '1em', lg: '5.8em' }} shadow='md'>
 			<Text fontSize='lg' fontWeight='extraBold'>
 				Where in the world?
 			</Text>
