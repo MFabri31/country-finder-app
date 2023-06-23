90b38ba1120ffb4aac008e8b22e5f1c284f0be32
MFabri31 <48441333+MFabri31@users.noreply.github.com>
2023-06-23 18:14:51 -0300

    Add link to go to home page on the logo

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,10 +1,11 @@
 import { Box, Text } from '@chakra-ui/react'
+import { Link } from 'react-router-dom'
 
 export const NavBar = () => {
 	return (
 		<Box py='2em' px={{ base: '1em', lg: '5.8em' }} shadow='md'>
 			<Text fontSize='lg' fontWeight='extraBold'>
-				Where in the world?
+				<Link to='/'>Where in the world?</Link>
 			</Text>
 		</Box>
 	)
