5c15654620b4dfb9a3c911c6bb1a0f69f57e20f7
MFabri31 <48441333+MFabri31@users.noreply.github.com>
2023-06-23 19:49:21 -0300

    Create country card

--- src/Pages/HomePage/HomePage.jsx
+++ src/Pages/HomePage/HomePage.jsx
@@ -1,23 +1,18 @@
 import { SearchForm } from '../../components/SearchForm/SearchForm'
-
-import {
-	SimpleGrid,
-	Card,
-	Image,
-	CardBody,
-	Heading,
-	Text,
-} from '@chakra-ui/react'
+import { SimpleGrid } from '@chakra-ui/react'
 import { useSearchForm } from '../../hooks/useSearchForm'
 import { useCountries } from '../../hooks/useCountries'
 import { Loader } from '../../components/Loader/Loader'
-import { Link } from 'react-router-dom'
+import { CountryCard } from '../../components/CountryCard/CountryCard'
 
 export const HomePage = () => {
 	const { searchValue, getFormValue, getSelectValue } = useSearchForm()
 
 	const { countries, loading } = useCountries({ searchValue })
 
+	const countryList = countries?.map(country => (
+		<CountryCard key={country.id} {...country} />
+	))
 	return (
 		<>
 			<SearchForm getFormValue={getFormValue} getSelectValue={getSelectValue} />
@@ -25,25 +20,7 @@ export const HomePage = () => {
 			{loading && <Loader />}
 
 			<SimpleGrid columns={{ md: '4' }} gap={10}>
-				{countries?.map(country => (
-					<Link key={country.id} to={`country/${country.name.common}`}>
-						<Card mb='1rem' maxW='sm' shadow='md' borderRadius='8px'>
-							<Image
-								src={country.flags.svg}
-								borderTopRadius='8px'
-								alt={country.flags.alt}
-							/>
-							<CardBody>
-								<Heading as='h3' size='lg'>
-									{country.name.common}
-								</Heading>
-								<Text>Population:{country.population}</Text>
-								<Text>Region: {country.region}</Text>
-								<Text>Capital: {country.capital}</Text>
-							</CardBody>
-						</Card>
-					</Link>
-				))}
+				{countryList}
 			</SimpleGrid>
 		</>
 	)
---
+++ src/components/CountryCard/CountryCard.jsx
@@ -0,0 +1,35 @@
+import { Card, Image, CardBody, Heading, Text, Box } from '@chakra-ui/react'
+import { Link } from 'react-router-dom'
+
+export const CountryCard = ({ name, flags, population, region, capital }) => {
+	return (
+		<Link to={`country/${name.common}`}>
+			<Card mb='1rem' maxW='sm' shadow='md' borderRadius='8px'>
+				<Image src={flags.svg} borderTopRadius='8px' alt={flags.alt} />
+				<CardBody>
+					<Heading as='h3' size='md' mb='1rem'>
+						{name.common}
+					</Heading>
+					<Text fontWeight='semiBold'>
+						Population: {''}
+						<Box as='span' fontWeight='light'>
+							{population}
+						</Box>
+					</Text>
+					<Text fontWeight='semiBold'>
+						Capital: {''}
+						<Box as='span' fontWeight='light'>
+							{capital}
+						</Box>
+					</Text>
+					<Text fontWeight='semiBold'>
+						Region:{' '}
+						<Box as='span' fontWeight='light'>
+							{region}
+						</Box>
+					</Text>
+				</CardBody>
+			</Card>
+		</Link>
+	)
+}
