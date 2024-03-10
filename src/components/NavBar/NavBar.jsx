ea7fa8ff5f6dcfd006c0788c5bb8eff38ea3d75f
MFabri31 <medinajose3195@gmail.com>
2024-03-10 04:47:44 -0300

    Add layout to the navbar

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,12 +1,15 @@
-import { Box, Text } from '@chakra-ui/react'
-import { Link } from 'react-router-dom'
+import { Box, Text } from "@chakra-ui/react";
+import { Link } from "react-router-dom";
+import { Layout } from "../Layout/Layout";
 
 export const NavBar = () => {
-	return (
-		<Box py='2em' px={{ base: '1em', lg: '5.8em' }} shadow='md'>
-			<Text fontSize='lg' fontWeight='extraBold'>
-				<Link to='/'>Where in the world?</Link>
-			</Text>
-		</Box>
-	)
-}
+  return (
+    <Box py="2em" px={{ base: "1em", lg: "5.8em" }} shadow="md">
+      <Layout>
+        <Text fontSize="lg" fontWeight="extraBold">
+          <Link to="/">Where in the world?</Link>
+        </Text>
+      </Layout>
+    </Box>
+  );
+};
