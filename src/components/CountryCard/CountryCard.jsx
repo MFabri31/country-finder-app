f486a7232b118613564cdee196d4ec1376c32564
mfabri31 <fabriciomedina9531@gmail.com>
2025-07-07 17:28:13 -0300

    Change the width of card image

--- src/components/CountryCard/CountryCard.jsx
+++ src/components/CountryCard/CountryCard.jsx
@@ -4,8 +4,28 @@ import { Link } from "react-router-dom";
 export const CountryCard = ({ name, flags, population, region, capital }) => {
   return (
     <Link to={`country/${name.common}`}>
-      <Card mb="1rem" maxW="auto" shadow="md" borderRadius="8px">
-        <Image src={flags.svg} borderTopRadius="8px" alt={flags.alt} />
+      <Card
+        mb="1rem"
+        w="18rem"
+        shadow="md"
+        borderRadius="8px"
+        overflow="hidden"
+      >
+        <Box
+          w="100%"
+          h="12rem"
+          bg="gray.100"
+          display="flex"
+          alignItems="center"
+          justifyContent="center"
+        >
+          <Image
+            src={flags.svg}
+            borderTopRadius="8px"
+            alt={flags.alt}
+            objectFit="contain"
+          />
+        </Box>
         <CardBody>
           <Heading as="h3" size="md" mb="1rem">
             {name.common}
